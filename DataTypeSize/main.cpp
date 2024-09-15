88cde88ba2ef544f3c12cc2b82a1fc1e6174c12a
Chatchapong Thuaypha <chatchapong1@windowslive.com>
2024-09-15 04:13:06 -0700

    Completed program using the codes by following the tutorial in class.

--- DataTypeSize/main.cpp
+++ DataTypeSize/main.cpp
@@ -1,14 +1,21 @@
-//
-//  main.cpp
-//  DataTypeSize
-//
-//  Created by Chatchapong Thuaypha on 9/15/24.
-//
-
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    
+    // Data type's size and limits
+    
+    // Shorts
+    cout << "The size of a short is: "<< sizeof(short) << endl;
+    cout << "The Max limit of a short is: " << numeric_limits<short>::max() << endl;
+    cout << "The Min limit of a short is: " << numeric_limits<short>::min() << endl;
+    
+    cout << endl;
+    
+    // Double
+    cout << "The size of a double is: "<< sizeof(double) << endl;
+    cout << "The Max limit of a double is: " << numeric_limits<double>::max() << endl;
+    cout << "The Min limit of a double is: " << numeric_limits<double>::min() << endl;
+    
     return 0;
 }
